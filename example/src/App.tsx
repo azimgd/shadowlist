9bac94107e49d7fba1706786ba875f0a9b415154
azim <me@azimgd.com>
2024-04-27 01:25:26 +0500

    chore: extend example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,17 +2,14 @@ import * as React from 'react';
 
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { CraigsListContainer, CraigsListItem } from 'react-native-craigs-list';
+import data from './data.json';
+
+const chats = Array(800).fill(data).flat();
 
 export default function App() {
   const craigsListContainerRef = React.useRef<{
     scrollToIndex: (index: number) => void;
   }>(null);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      craigsListContainerRef.current?.scrollToIndex(800);
-    }, 5000);
-  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -20,9 +17,10 @@ export default function App() {
         style={styles.container}
         ref={craigsListContainerRef}
       >
-        {Array.from(Array(1000).keys()).map((item) => (
-          <CraigsListItem key={item} style={styles.item}>
-            <Text style={styles.text}>item → {item}</Text>
+        {chats.map((item, index) => (
+          <CraigsListItem key={index} style={styles.item}>
+            <Text style={styles.username}>{item.username}</Text>
+            <Text style={styles.text}>{item.text}</Text>
           </CraigsListItem>
         ))}
       </CraigsListContainer>
@@ -36,15 +34,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#74b9ff',
   },
   item: {
-    height: 50,
-    paddingHorizontal: 40,
+    padding: 24,
     justifyContent: 'center',
     backgroundColor: '#0984e3',
     borderBottomColor: '#74b9ff',
     borderBottomWidth: 1,
   },
-  text: {
+  username: {
     color: '#ffffff',
     fontWeight: '600',
+    paddingBottom: 12,
+  },
+  text: {
+    color: '#ffffff',
   },
 });
