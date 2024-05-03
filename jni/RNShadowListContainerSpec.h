45861d7e63ce7050a3489f830e0a18d523d4312f
azim <me@azimgd.com>
2024-05-03 06:26:15 +0500

    chore: custom descriptor definition

--- jni/RNShadowListContainerSpec.h
+++ jni/RNShadowListContainerSpec.h
@@ -3,6 +3,7 @@
 #include <ReactCommon/JavaTurboModule.h>
 #include <ReactCommon/TurboModule.h>
 #include <jsi/jsi.h>
+#include <react/renderer/components/RNShadowListContainerSpec/ComponentDescriptors.h>
 
 namespace facebook::react {
 
--- react-native.config.js
+++ react-native.config.js
@@ -4,7 +4,7 @@ module.exports = {
       android: {
         componentDescriptors: [
           'ShadowListContainerComponentDescriptor',
-          'ShadowListItemComponentDescriptor',
+          // 'ShadowListItemComponentDescriptor',
         ],
         cmakeListsPath: '../jni/CMakeLists.txt',
       },
