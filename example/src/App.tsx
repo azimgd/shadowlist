5698bd975bfa0aa664d493571221aa199ef57c8e
azim <me@azimgd.com>
2024-11-11 09:26:44 +0500

    chore: flatlist api compatibility progress

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,47 @@
 import { useRef, useCallback } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
-import { SLContainer, type SLContainerRef } from 'shadowlist';
+import {
+  Shadowlist,
+  type ShadowlistProps,
+  type SLContainerRef,
+} from 'shadowlist';
+
+const ListHeaderComponent = () => <Text style={styles.text}>Header</Text>;
+const ListFooterComponent = () => <Text style={styles.text}>Footer</Text>;
+
+const renderItem: ShadowlistProps['renderItem'] = ({ item }) => {
+  const text = `Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. Lorem Ipsum has been the industry's standard dummy text ever ever
+since the 1500s, when an unknown printer took a galley of type and scrambled
+it to make a type specimen book. It has survived not only five centuries,
+but also the leap into electronic typesetting, remaining essentially
+unchanged. It was popularised in the 1960s with with the release of Letraset
+sheets containing Lorem Ipsum passages, and more recently with desktop
+publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+
+  return (
+    <Text style={styles.text} key={item}>
+      {item} {text.substring(0, Math.random() * 1000)}
+    </Text>
+  );
+};
 
 export default function App() {
   const ref = useRef<SLContainerRef>(null);
   const onVisibleChange = useCallback(() => {}, []);
+  const data = Array.from({ length: 1000 }, (_, item) => item);
+  ref.current?.scrollToIndex({ index: 5 });
 
   return (
     <SafeAreaView style={styles.container}>
-      <SLContainer
-        style={styles.content}
+      <Shadowlist
         ref={ref}
+        renderItem={renderItem}
+        data={data}
         onVisibleChange={onVisibleChange}
-      >
-        {Array.from({ length: 1000 }, (_, i) => (
-          <Text style={styles.text} key={i}>
-            {i} Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </Text>
-        ))}
-      </SLContainer>
+        ListHeaderComponent={ListHeaderComponent}
+        ListFooterComponent={ListFooterComponent}
+      />
     </SafeAreaView>
   );
 }
---
+++ src/SLContainer.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import SLContainerNativeComponent, {
+  Commands,
+  type ScrollToIndexOptions,
+  type ScrollToOffsetOptions,
+  type SLContainerNativeProps,
+  type SLContainerNativeCommands,
+} from './SLContainerNativeComponent';
+
+export type SLContainerWrapperProps = {};
+
+export type SLContainerInstance = InstanceType<
+  typeof SLContainerNativeComponent
+>;
+
+export type SLContainerRef = {
+  scrollToIndex: (options: ScrollToIndexOptions) => void;
+  scrollToOffset: (offset: ScrollToOffsetOptions) => void;
+};
+
+const SLContainerWrapper = (
+  props: SLContainerNativeProps & SLContainerWrapperProps,
+  forwardedRef: React.Ref<Partial<SLContainerNativeCommands>>
+) => {
+  const instanceRef = React.useRef<SLContainerInstance>(null);
+
+  React.useImperativeHandle<Partial<SLContainerNativeCommands>, SLContainerRef>(
+    forwardedRef,
+    () => ({
+      scrollToIndex: (options: ScrollToIndexOptions) => {
+        Commands.scrollToIndex(
+          instanceRef.current as never,
+          options.index,
+          options.animated || false
+        );
+      },
+      scrollToOffset: (options: ScrollToOffsetOptions) => {
+        Commands.scrollToOffset(
+          instanceRef.current as never,
+          options.offset,
+          options.animated || false
+        );
+      },
+    })
+  );
+
+  const containerStyle = props.horizontal
+    ? styles.containerHorizontal
+    : styles.containerVertical;
+
+  return (
+    <SLContainerNativeComponent
+      {...props}
+      ref={instanceRef}
+      style={[containerStyle, props.style]}
+    >
+      {props.children}
+    </SLContainerNativeComponent>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerVertical: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  containerHorizontal: {
+    flex: 0,
+    flexDirection: 'row',
+  },
+});
+
+export const SLContainer = React.forwardRef(SLContainerWrapper);
