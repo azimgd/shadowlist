04405440b79c54e0c6de6fd5bdd8aef439046220
azim <me@azimgd.com>
2024-11-26 04:44:33 +0500

    chore: fix rctcomponentviewprotocol import

--- ios/SLContainerChildrenManager.h
+++ ios/SLContainerChildrenManager.h
@@ -3,7 +3,7 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "RCTComponentViewProtocol.h"
+#import <React/RCTComponentViewProtocol.h>
 #import "SLComponentRegistry.h"
 
 @interface SLContainerChildrenManager : NSObject
