4a2de2451c7299ec0ab1b3dd80b708c9dc8be1f0
azim <me@azimgd.com>
2024-12-30 06:22:43 +0500

    chore: add performance metrics and onlayout event dispatcher

--- cpp/react/renderer/components/SLContainerSpec/SLContainerShadowNode.cpp
+++ cpp/react/renderer/components/SLContainerSpec/SLContainerShadowNode.cpp
@@ -16,6 +16,10 @@ struct ComponentRegistryItem {
 };
 
 void SLContainerShadowNode::layout(LayoutContext layoutContext) {
+  #ifndef RCT_DEBUG
+  auto start = std::chrono::high_resolution_clock::now();
+  #endif
+
   auto &props = getConcreteProps();
   auto nextStateData = getStateData();
 
@@ -24,7 +28,10 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
 
   nextStateData.scrollPositionUpdated = false;
 
-  auto containerShadowNodeChildren = std::make_shared<ShadowNode::ListOfShared>(*ShadowNode::emptySharedShadowNodeSharedList());
+  /*
+   * Store the child nodes for the container
+   */
+  auto containerShadowNodeChildren = std::make_shared<ShadowNode::ListOfShared>();
 
   bool elementsDataPrepended = elementsDataSize && nextStateData.firstChildUniqueId.size() &&
     nextStateData.firstChildUniqueId != props.uniqueIds.front();
@@ -32,7 +39,7 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     nextStateData.lastChildUniqueId != props.uniqueIds.back();
 
   /*
-   *
+   * Find the index of the first child element that has not been measured
    */
   if (nextStateData.childrenMeasurementsTree.size() && props.data.size() != nextStateData.childrenMeasurementsTree.size()) {
     for (int elementDataIndex = 0; elementDataIndex < props.data.size(); ++elementDataIndex) {
@@ -49,7 +56,8 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
   }
 
   /*
-   *
+   * If data is prepended, extend the measurements tree from the beginning 
+   * to match the new data size. If data is appended, resize the tree at the end
    */
   if (elementsDataPrepended) {
     SLFenwickTree childrenMeasurementsTreeNext{};
@@ -66,6 +74,11 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     nextStateData.childrenMeasurementsTree.resize(elementsDataSize);
   }
 
+  /*
+   * Adjust the scroll index when the user is near the top of the container to load more data,
+   * and updates the scrollPositionUpdated flag to keep track of changes in the list position,
+   * ensuring the visible content stays in the correct place
+   */
   if (nextStateData.scrollPosition.y < nextStateData.scrollContainer.height) {
     if (std::max(nextStateData.scrollIndex - 10, 0) != nextStateData.scrollIndex) {
       nextStateData.scrollIndex = std::max(nextStateData.scrollIndex - 10, 0);
@@ -77,14 +90,16 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
       getConcreteEventEmitter().onStartReached({ .distanceFromStart = distanceFromStart });
     }
   }
-  
+
   if (nextStateData.scrollContainer.height == getLayoutMetrics().frame.size.height && nextStateData.scrollPosition.y >= nextStateData.scrollContent.height - nextStateData.scrollContainer.height - 1) {
     int distanceFromEnd = nextStateData.scrollContainer.height - getLayoutMetrics().frame.size.height;
     getConcreteEventEmitter().onEndReached({ .distanceFromEnd = distanceFromEnd });
   }
 
   /*
-   * Transformer
+   * Transforms the element's data into a cached reference to the element instance
+   * Clones a new shadow node from a template and adds it to the registry if not already present
+   * Measures the layout and stores the height in a measurement tree
    */
   auto transform = [&](int elementDataIndex) -> ComponentRegistryItem {
     auto elementDataUniqueKey = props.uniqueIds[elementDataIndex];
@@ -99,6 +114,7 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
 
     auto elementShadowNodeLayoutable = std::static_pointer_cast<YogaLayoutableShadowNode>(elementShadowNodeComponentRegistry[elementDataUniqueKey]);
 
+    // Prevent re-measuring if the height is already defined, as layouting is expensive
     if (elementShadowNodeLayoutable->getLayoutMetrics().frame.size.height == 0) {
       LayoutConstraints layoutConstraints = {};
       layoutConstraints.minimumSize.width = getLayoutMetrics().frame.size.width;
@@ -115,6 +131,9 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     };
   };
 
+  /*
+   * Calculate sequence of indices above and below the current scroll index
+   */
   int scrollContentAboveIndex = 0;
   float scrollContentAboveOffset = 0;
   auto scrollContentAboveComponents = std::views::iota(0, nextStateData.scrollIndex)
@@ -123,6 +142,10 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     | std::views::reverse
     | std::views::transform(transform);
 
+  /*
+   * Start from the scroll index and continues until it reaches the end or the next item is
+   * no longer visible on the screen
+   */
   int scrollContentBelowIndex = 0;
   float scrollContentBelowOffset = 0;
   auto scrollContentBelowComponents = std::views::iota(nextStateData.scrollIndex, elementsDataSize)
@@ -132,6 +155,10 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     })
     | std::views::transform(transform);
 
+  /*
+   * Iterate through the components above the scroll content, update their position,
+   * and add them to the container if they are visible in the current viewport
+   */
   for (ComponentRegistryItem componentRegistryItem : scrollContentAboveComponents) {
     auto elementShadowNodeLayoutable = std::static_pointer_cast<YogaLayoutableShadowNode>(elementShadowNodeComponentRegistry[componentRegistryItem.elementDataUniqueKey]);
     LayoutMetrics layoutMetrics = elementShadowNodeLayoutable->getLayoutMetrics();
@@ -147,6 +174,10 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     }
   }
 
+  /*
+   * Go through the components below the scroll content, update their position,
+   * and add them to the container if they are visible in the current viewport
+   */
   for (ComponentRegistryItem componentRegistryItem : scrollContentBelowComponents) {
     auto elementShadowNodeLayoutable = std::static_pointer_cast<YogaLayoutableShadowNode>(elementShadowNodeComponentRegistry[componentRegistryItem.elementDataUniqueKey]);
     LayoutMetrics layoutMetrics = elementShadowNodeLayoutable->getLayoutMetrics();
@@ -162,6 +193,9 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
     }
   }
 
+  /*
+   * Update children and mark the container as dirty to trigger a layout update on state change
+   */
   this->children_ = containerShadowNodeChildren;
   yogaNode_.setDirty(true);
 
@@ -172,12 +206,26 @@ void SLContainerShadowNode::layout(LayoutContext layoutContext) {
   nextStateData.scrollContent.width = getLayoutMetrics().frame.size.width;
   nextStateData.scrollContent.height = nextStateData.childrenMeasurementsTree.sum(elementsDataSize);
 
+  /*
+   * Update the scroll position when new items are prepended to the top of the list
+   */
   if (nextStateData.scrollPositionUpdated) {
     nextStateData.scrollPosition.x = 0;
     nextStateData.scrollPosition.y = scrollContentAboveOffset + nextStateData.scrollPosition.y;
   }
 
+  getConcreteEventEmitter().onVisibleChange({
+    .visibleStartIndex = scrollContentBelowIndex,
+    .visibleEndIndex = scrollContentAboveIndex,
+  });
+
   setStateData(std::move(nextStateData));
+  
+  #ifndef RCT_DEBUG
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "SLContainerShadowNode onLayout: " << std::fixed << std::setprecision(2) << duration.count() << " ms" << std::endl;
+  #endif
 }
 
 void SLContainerShadowNode::appendChild(const ShadowNode::Shared& child) {
