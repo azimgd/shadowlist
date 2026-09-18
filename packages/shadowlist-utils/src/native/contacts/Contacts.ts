460a073d8d0ae696ff1ddd90f4631265182202d5
azimgd <me@azimgd.com>
2026-09-18 11:55:42 +0500

    contacts: fold the section kit in and share a row renderer hook

---
+++ packages/shadowlist-utils/src/native/contacts/Contacts.ts
@@ -0,0 +1,9 @@
+import { ContactRow } from './ContactRow';
+import { ContactsList } from './ContactsList';
+import { ContactsSectionList } from './ContactsSectionList';
+
+export const Contacts = {
+  List: ContactsList,
+  SectionList: ContactsSectionList,
+  Row: ContactRow,
+};
--- packages/shadowlist-utils/src/native/contacts/index.ts
+++ packages/shadowlist-utils/src/native/contacts/index.ts
@@ -1,10 +1,13 @@
-import { ContactsList } from './ContactsList';
-import { ContactRow } from './ContactRow';
+export { Contacts } from './Contacts';
+export { defaultContactsLabels } from './labels';
 
 export type { ContactsListProps } from './ContactsList';
+export type {
+  ContactsSectionListProps,
+  ContactSection,
+  ContactSectionMeta,
+} from './ContactsSectionList';
 export type { ContactRowProps } from './ContactRow';
-
-export const Contacts = {
-  List: ContactsList,
-  Row: ContactRow,
-};
+export type { ContactRowOptions } from './useContactRowRenderer';
+export type { ContactsLabels } from './labels';
+export type { ContactItem } from './types';
